8321c67d8f60688d44c614d3025235f54bd011a5
policelinemessbbaria <bpsujon0842@gmail.com>
2026-06-27 09:39:47 +0600

    Change CACHE_NAME to police-phonebook-v6
    Updated cache version from v5 to v6.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'police-phonebook-v5'; // ভার্সন চেঞ্জ করেছি (v1 থেকে v2)
+const CACHE_NAME = 'police-phonebook-v6'; // ভার্সন চেঞ্জ করেছি (v1 থেকে v2)
 
 const ASSETS = [
   './',
